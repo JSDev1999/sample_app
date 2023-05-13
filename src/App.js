c4633122964505b378ff0980fb307b18fda61b0e
JSDev1999 <rameshlearningadvance@gmail.com>
2023-05-13 12:00:36 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./styles.css";
 
 export default function App() {
   const name = "Ramesh";
+  const age = 25
   const [state, setState] = useState(0);
   return (
     <div className="App">
-      <h1>Hello {name}</h1>
+      <h1>Hello {name}, your age is {age}</h1>
       <h2>the number is : {state}</h2>
       <button onClick={() => setState((prev) => prev + 1)}>plus</button>
       <button onClick={() => setState((prev) => prev - 1)}>minus</button>
